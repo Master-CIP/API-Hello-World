7acc2ac58775af4eb5b851e999fce5da663fb5bf
YASSER-CSR <allaouiyasser7@gmail.com>
2025-05-11 15:39:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
 });
 
 app.listen(port, () => {
-  console.log(`API running at http://localhost:${port}/hello`);
+  console.log(`API Hello World en écoute sur http://localhost:${port}`);
 });
